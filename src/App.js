71023e35261a288a4b64a54287cc2faf53920be4
Daniel Voicu <daniel.voicu@brandfusion.ro>
2019-12-20 16:57:06 +0200

    test suspense

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+const Header = React.lazy(() => import('./modules/Header'));
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Header />
+      </Suspense>               
       </header>
     </div>
   );
