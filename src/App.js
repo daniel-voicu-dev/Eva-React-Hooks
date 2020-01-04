c1cf49b258f96f615f283957e37d0b0774302af8
Daniel Voicu <>
2020-01-04 03:04:52 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { lazy, Suspense } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
 const Header = React.lazy(() => import('./modules/Header'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App">     
       <Suspense fallback={<div>Loading...</div>}>
         <Header />
-      </Suspense>               
-      </header>
+      </Suspense>   
     </div>
   );
 }
--- src/modules/Header.js
+++ src/modules/Header.js
@@ -1,10 +1,25 @@
 import React from 'react'
 
+import "../sass/header.sass"
+
 const Header = () => {
   return (
-    <div>
-      <h1>This is the header</h1>
-    </div>
+    <header className="header grid g-3-1-2"> 
+      <div className="logo g-1"><img src="https://placeholder.com/wp-content/uploads/2018/10/placeholder.com-logo1.png" alt="Logo" /></div>
+      <div className="nav--wrapper g-2">
+        <div className="search"><input type="text" /> <button type="button">Search</button></div>
+        <nav className="nav--header">        
+          <a href="/"><i className="ion-md-person"></i></a>
+          <a href="/"><i className="ion-md-heart"></i></a>
+          <a href="/"><i className="ion-md-cart"></i></a>        
+        </nav>
+      </div>      
+      <nav className="nav--secondary g-3">       
+        <a href="/">US</a>
+        <a href="/">Europe</a>
+        <a href="/">Asia</a>       
+      </nav>
+    </header>
   )
 }
 
